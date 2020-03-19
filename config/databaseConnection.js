84d052d2b5b6fe90654341e0fa1110fcfa5bf03e
Neeraj kumar <neeraj.kumar@mountblue.tech>
2020-03-19 07:28:19 +0530

    changes in database connection name

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('movieAndDirectorApi', 'neeraj', 'neeraj12', {
-    host: 'localhost',
+module.exports = new Sequelize('d6op1plpnlminb', 'ogkmkmsdjodeoh', 'd4f64cc2e18c5d229272d98675f6b084b152ab9d57ce8bb840ddb07eaa28e319', {
+    host: 'ec2-50-17-178-87.compute-1.amazonaws.com',
     dialect: 'postgres'
 });
+
